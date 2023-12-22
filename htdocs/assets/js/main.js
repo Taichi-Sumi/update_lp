0901642fb127544b6eceea96e7ee5e83d39ca668
Taichi-Sumi <taichi.sumi.6@gmail.com>
2023-12-22 16:37:50 +0900

    画像の明度アップ

--- htdocs/assets/js/main.js
+++ htdocs/assets/js/main.js
@@ -53,7 +53,7 @@ $('section:has(.acco-title)').on('click', function () {//タイトル要素を�
     // let findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
     let findElm = $(this).children(".box")
         ;//直後のアコーディオンを行うエリアを取得し
-    $(findElm).slideToggle();//アコーディオンの上下動作
+    $(findElm).stop().slideToggle();//アコーディオンの上下動作
 
     if ($(this).hasClass('close')) {//タイトル要素にクラス名closeがあれば
         $(this).removeClass('close');//クラス名を除去し
